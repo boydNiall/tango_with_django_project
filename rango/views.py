236c8ec9546c32f01240d24a8ed9ab6d0c3f7ccd
Niall Boyd <2392483B@student.gla.ac.uk>
2020-01-29 15:13:05 +0000

    chapter 4

---
+++ rango/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    #construct a dictionary to pass to the template engine as its context
+    #Note the key boldmessage matches to {{ boldmessage}} in the template!
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    #Return a rendered response to send to the client
+    #We make use of the shortcut function to make our lives easier
+    #Note that first parameter is the template we want to use
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    context_dict = {'boldmessage': 'This tutorial has been put together by Niall Boyd.'}
+    return render(request, 'rango/about.html', context=context_dict)
